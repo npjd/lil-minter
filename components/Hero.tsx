5ab8617b4693e16775328ffcf8101638ef281a6b
Nima Pourjafar <47256203+nimapourjafar@users.noreply.github.com>
2022-05-31 22:01:00 -0400

    character error when deploying

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <>
       <h1 className="text-6xl font-bold">
-        Lil' <span className="text-blue-600">Minter</span>
+        Lil <span className="text-blue-600">Minter</span>
       </h1>
 
       <p className="mt-3 text-2xl">
