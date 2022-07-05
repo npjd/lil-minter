c05e8a8bf1ba8f8d2b13cb961f172040e0857e6d
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-07-05 15:27:09 -0400

    changed footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,16 @@ import Image from 'next/image'
 export default function Footer() {
   return (
     <footer className="flex h-24 w-full items-center justify-center border-t mt-8 absolute bottom-0">
+      <h1> Something not working? Submit bugs/issues {" "}
         <a
           className="underline text-blue-500 hover:text-blue-800 visited:text-purple-600 hover:cursor-pointer"
           target="_blank"
           rel="noopener noreferrer"
           href="https://github.com/nimapourjafar/lil-minter"
         >
-          Source Code
+          {" "}here
         </a>
+        </h1>
       </footer>
   )
 }
