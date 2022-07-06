f0409c7ff825c7b598cd214d9bfa301681becd10
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-07-05 22:18:40 -0400

    updated favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col py-2 h-full">
       <Head>
         <title>Lil Minter</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖼</text></svg>"/>
       </Head>
 
       <nav>
